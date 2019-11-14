5fc58ae0a646536530ed4e6e167f990a0d848453
chenxiong <chenxiong@addcn.com>
2019-11-14 18:27:13 +0800

    [release] 0.0.24

--- packages/index.js
+++ packages/index.js
@@ -42,7 +42,7 @@ import Uploader from './uploader';
 import VAnchor from './v-anchor';
 import VClickOutside from './v-click-outside';
 
-const version = '0.0.23';
+const version = '0.0.24';
 const components = [
   Button,
   Card,
