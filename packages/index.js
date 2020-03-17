1bb92b48290b7b097fdb6c67ec727a02a98fba53
chenxiong <chenxiong@addcn.com>
2020-03-17 17:03:24 +0800

    [release] 0.0.1

--- packages/index.js
+++ packages/index.js
@@ -41,7 +41,7 @@ import Uploader from './uploader';
 import VAnchor from './v-anchor';
 import VClickOutside from './v-click-outside';
 
-const version = '0.0.35';
+const version = '0.0.1';
 const components = [
   Button,
   Card,
