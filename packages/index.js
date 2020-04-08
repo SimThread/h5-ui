2337917d741360a1f705be1ac10788004d01852e
chenxiong <chenxiong@addcn.com>
2020-04-08 14:39:47 +0800

    [release] 0.0.6

--- packages/index.js
+++ packages/index.js
@@ -44,7 +44,7 @@ import VAnchor from './v-anchor';
 import VClickOutside from './v-click-outside';
 import Locale from './_locale';
 
-const version = '0.0.5';
+const version = '0.0.6';
 const components = [
   Button,
   Card,
