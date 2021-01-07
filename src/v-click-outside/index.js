1310c47e1cbd5e4e7fcfd0b401a0243d87e5fcfc
chenxiong <chenxiong@addcn.com>
2021-01-07 19:16:24 +0800

    npm 安装测试

--- src/v-click-outside/index.js
+++ src/v-click-outside/index.js
@@ -1,6 +1,7 @@
 import ClickOutside from '../directives/click-outside';
 
 ClickOutside.install = function(Vue) {
+    console.log('npm 安装测试');
     Vue.directive(ClickOutside.name, ClickOutside);
 };
 export default ClickOutside;
